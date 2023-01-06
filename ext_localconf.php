6c4df41b007d2f30d7adbd946f9b25d12cddcd2e
thomas.layh <thomas.layh@aoe.com>
2023-01-06 08:33:43 +0100

    [DEPRECATION] remove deprecated vendor name

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,11 +1,13 @@
 <?php
+
 use Aoe\GoogleTagManager\Controller\DataLayerController;
 use Aoe\GoogleTagManager\Controller\TrackingController;
+use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 
 defined('TYPO3') or die();
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'Aoe.google_tag_manager',
+ExtensionUtility::configurePlugin(
+    'google_tag_manager',
     'Tracking',
     [
         TrackingController::class => 'index,values'
@@ -15,8 +17,8 @@ defined('TYPO3') or die();
     ]
 );
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'Aoe.google_tag_manager',
+ExtensionUtility::configurePlugin(
+    'google_tag_manager',
     'DataLayer',
     [
         DataLayerController::class => 'index'
